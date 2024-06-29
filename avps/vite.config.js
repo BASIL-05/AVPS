83a83b87340333fefb1d7adbd06c4d2b2b8a0def
aneef-s <aneefchulu205@gmail.com>
2024-06-29 22:36:01 +0530

    Update Vite Config

--- avps/vite.config.js
+++ avps/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-icons/fa'],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['react-icons/fa'],
+  //   },
+    base:"./",
+  
 });
