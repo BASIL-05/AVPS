a61945c716b89d18b73e5cd9d28c3b2bf8f3d0f1
BASIL SABU <basilsabu5556@gmail.com>
2024-07-01 11:55:47 +0530

    Update Hero.jsx

--- avps/src/components/Hero.jsx
+++ avps/src/components/Hero.jsx
@@ -15,19 +15,19 @@ function Hero() {
                             <Link to={{pathname:'/VehicleList'}} state='Vehicle Inside'>
                                 <div className="bg-blue-200 text-blue-900 p-4 rounded-lg shadow-lg">
                                     <p className="text-sm">Vehicles Inside</p>
-                                    <p className="text-4xl font-bold">123</p>
+                                    <p className="text-4xl font-bold">001</p>
                                 </div>
                             </Link>
                             <Link to={{ pathname: '/VehicleList'}} state='Total Visit'>
                                 <div className="bg-green-200 text-green-900 p-4 rounded-lg shadow-lg">
                                     <p className="text-sm">Total Visit</p>
-                                    <p className="text-4xl font-bold">456</p>
+                                    <p className="text-4xl font-bold">106</p>
                                 </div>
                             </Link>
                             <Link to={{ pathname: '/VehicleList'}} state='Guests Inside'>
                                 <div className="bg-yellow-200 text-yellow-900 p-4 rounded-lg shadow-lg">
                                     <p className="text-sm">Guests Inside</p>
-                                    <p className="text-4xl font-bold">789</p>
+                                    <p className="text-4xl font-bold">008</p>
                                 </div>
                             </Link>
                         </div>
